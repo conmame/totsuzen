61c1190c5b91c7fe2c0880d6079c093a57bf04e3
con_mame <con_mame@conma.me>
2012-07-15 15:07:26 +0900

    update version

--- spec/totsuzen_spec.rb
+++ spec/totsuzen_spec.rb
@@ -3,7 +3,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Totsuzen do
-  it { Totsuzen::VERSION.should eq '0.0.2' }
+  it { Totsuzen::VERSION.should eq '0.0.3' }
 
   describe "totsuzen method" do
     it 'make totsuzen in English' do
