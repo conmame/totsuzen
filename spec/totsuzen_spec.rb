e91903c712a970f42e24fa89e4116f5c630e872b
TAKANO Mitsuhiro <takano32@gmail.com>
2016-06-11 14:23:36 +0900

    fix totsuzen spec

--- spec/totsuzen_spec.rb
+++ spec/totsuzen_spec.rb
@@ -7,33 +7,45 @@ describe Totsuzen do
 
   describe "totsuzen method" do
     it 'make totsuzen in English' do
-      "hoge".totsuzen.should == "＿人人人人＿
-＞ hoge ＜
-￣^Y^Y^Y^Y^￣"
+      t = <<-EOT
+＿人人人人＿
+＞　hoge　＜
+￣Y^Y^Y￣
+      EOT
+      "hoge".totsuzen.should == t.chomp
     end
 
     it 'make totsuzen in Japanaes' do
-      "こんまめ".totsuzen.should == "＿人人人人＿
-＞ こんまめ ＜
-￣^Y^Y^Y^Y^￣"
+      t = <<-EOT
+＿人人人人人人＿
+＞　こんまめ　＜
+￣Y^Y^Y^Y^Y￣
+      EOT
+      "こんまめ".totsuzen.should == t.chomp
     end
   end
 
   describe "totsuzen! method" do
     it 'make totsuzen! in English' do
       result = "hoge"
+      t = <<-EOT
+＿人人人人＿
+＞　hoge　＜
+￣Y^Y^Y￣
+      EOT
       result.totsuzen!
-      result.should == "＿人人人人＿
-＞ hoge ＜
-￣^Y^Y^Y^Y^￣"
+      result.should == t.chomp
     end
 
     it 'make totsuzen! in Japanes' do
       result = "こんまめ"
+      t = <<-EOT
+＿人人人人人人＿
+＞　こんまめ　＜
+￣Y^Y^Y^Y^Y￣
+      EOT
       result.totsuzen!
-      result.should == "＿人人人人＿
-＞ こんまめ ＜
-￣^Y^Y^Y^Y^￣"
+      result.should == t.chomp
     end
   end
 end
